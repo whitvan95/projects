8f2893b82d4995d7ca636146b946d618bb4c5396
Whitney Van Wagenen <whitvan95@gmail.com>
2016-02-21 22:35:31 -0700

    If statements and Booleans

--- project1/New Unity Project/Assets/UsingVariablesScript.cs
+++ project1/New Unity Project/Assets/UsingVariablesScript.cs
@@ -5,12 +5,18 @@ public class UsingVariablesScript : MonoBehaviour {
     // This variable is used to set the starting position of the cube
     public int StartingPosition = 5;
 
+    //Store if the cube is greater than zero
+    bool PositionGreaterThanZero;
+
 	// Use this for initialization
 	void Start () {
         // This is a local variable to temporarily store and manipulate the transform position
         Vector3 Position = transform.position;
         Position.y = StartingPosition;
         transform.position = Position;
+
+        PositionGreaterThanZero = true;
+
 	}
 	
 	// Update is called once per frame
@@ -22,18 +28,36 @@ public class UsingVariablesScript : MonoBehaviour {
     void DropToZero ()
     {
         // Only work if above zero
-        if (transform.position.y > 0)
+        if (PositionGreaterThanZero)
         {
             Vector3 Position = transform.position;
-            
+
             // Use the change in time to drop the cube 
             Position.y -= Time.deltaTime;
-            
+
             // If it's below zero make it equal to zero
             if (Position.y < 0)
             {
                 Position.y = 0;
+                PositionGreaterThanZero = false;
             }
+
+            transform.position = Position;
+        }
+
+        else
+        {
+            Vector3 Position = transform.position;
+
+            // Use the change in time to drop the cube 
+            Position.x -= Time.deltaTime;
+
+            // If it's below zero make it equal to zero
+            if (Position.x < -2)
+            {
+                Position.x = -2;
+            }
+
             transform.position = Position;
         }
     }
